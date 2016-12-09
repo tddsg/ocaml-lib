b14e450887ab450b66d735da73c411cf407f4bbd
tddsg <thedaydreamersg@gmail.com>
2016-12-09 14:05:35 +0800

    update

---
+++ src/tlist.ml
@@ -0,0 +1,20 @@
+(* List operations *)
+
+
+(** generate combination of k elements from a list *)
+let gen_combination (k: int) (lst : 'a list) : 'a list =
+  (* TODO *)
+  []
+
+
+(** generate permutations of a list *)
+let gen_permuatation (lst : 'a list) : 'a list =
+  (* TODO *)
+  []
+
+
+(** generate partitions of a list *)
+let gen_partitions (pred : 'a -> bool) (lst : 'a list) :
+  ('a list * 'a list) list =
+  (* TODO *)
+  []
