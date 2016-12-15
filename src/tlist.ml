665e8051f0a6943ebebd8af1c40dbdc57ec42c89
tddsg <thedaydreamersg@gmail.com>
2016-12-15 11:40:49 +0800

    exists, forall pairs

--- src/tlist.ml
+++ src/tlist.ml
@@ -18,3 +18,14 @@ let gen_partitions (pred : 'a -> bool) (lst : 'a list) :
   ('a list * 'a list) list =
   (* TODO *)
   []
+
+
+(** Exists a pair of elements from two list satisfying a given condition *)
+let exists_pair (f: 'a -> 'b -> bool) (lst1: 'a list) (lst2: 'b list) : bool =
+  List.exists (fun x ->
+      List.exists (fun y -> f x y) lst2) lst1
+
+(** All pairs of elements from two list satisfying a given condition *)
+let forall_pair (f: 'a -> 'b -> bool) (lst1: 'a list) (lst2: 'b list) : bool =
+  List.for_all (fun x ->
+      List.for_all (fun y -> f x y) lst2) lst1
